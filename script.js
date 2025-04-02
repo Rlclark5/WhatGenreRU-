454527d5c5c7da40aa1f443f9fbd9b857bdd58fb
Rlclark5 <rlclark5@cps.edu>
2025-04-02 16:16:21 +0000

    sec

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 console.log("hello");
 
-let age = document.querySelector(".age");
-console.log(age);
-let dog = document.querySelector(".dog");
-console.log(dog);
-let quiz = document.querySelector("quiz");
-console.log(quiz);
-let = age = Number(age.value);
-console.log(age);
-let = dog = dog.value;
-console.log(dog);
+let answer1 = document.querySelector(".answer1");
+console.log(answer1);
+let answer2 = document.querySelector(".answer2");
+console.log(answer2);
+let enter = document.querySelector(".enter");
+console.log(enter);
+let answer = document.querySelector(".answer");
+console.log(answer);
 
-quiz.onclick = function(){
+
+enter.onclick = function(){
     console.log("clicked");
-};
+    answer1 = answer1.value;
+console.log(answer1);
+answer2 = answer2.value;
+console.log(answer2);
 
-if(age === "7" && dog === "pup"){
-    console.log("shaggy");
-} else if(age === "" && dog === ""){
-    console.log("shaggy")
+if(answer1 === "Structured" && answer2 === "Beethoven"){
+  answer.innerHTML = "You’re a classicalist. You loved sight reading and stressing 25/8. REFRESH AND TRY AGAIN";
+    console.log("one");
+} else if(answer1 === "Improvise" && answer2 === "Beethoven"){
+    answer.innerHTML = "You prefer jazz music but you have a little structured side. I like that. REFRESH AND TRY AGAIN";
+} else if(answer1 === "Structured" && answer2 === "Miles Davis"){
+    answer.innerHTML = "You prefer classical music but you have a little funk to ya. I like that. REFRESH AND TRY AGAIN";
+} else if(answer1 === "Improvise" && answer2 === "Miles Davis"){
+    answer.innerHTML = "You’re into the jazz scenery I see. Let me hear a solo! REFRESH AND TRY AGAIN";
 }
+};
